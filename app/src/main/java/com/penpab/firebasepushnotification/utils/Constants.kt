ce0fd15cd5177471a9c1a62c3ec80a7449e6ba20
Acekhing <charlesannorblay@gmail.com>
2022-08-19 13:21:59 +0000

    fixed bug

--- app/src/main/java/com/penpab/firebasepushnotification/utils/Constants.kt
+++ app/src/main/java/com/penpab/firebasepushnotification/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.penpab.firebasepushnotification.utils
 object Constants {
     const val CONTENT_TYPE = "application/json"
     const val BASE_URL = "https://fcm.googleapis.com"
-    const val SERVERKEY = "AAAAy1WUDkk:APA91bGg4oiwaG_LS-3HyvdhzsL6qV4NPlyiP-KKjYSvPXXQp17UgRTz9Yh64BlRFDhw4VyniqH3hW9BXqz_HQfXeXL0xyo3EdkM76Oq5th5Vh7_aCV0ckplUrPmCHkkJtZDzG7sdvRl"
+    const val SERVERKEY = "** YOUR FCM SERVER_KEY **"
 }
